26e9882e37f03663ecdffa47a767e2b14ff9886f
jjh <1414197421@qq.com>
2023-11-29 19:12:33 +0800

    first commit

---
+++ components/common/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="mongo-btn">
+    <slot></slot>
+  </button>
+</template>
+<style scoped lang="postcss">
+  .mongo-btn {
+    @apply inline-block 
+    whitespace-nowrap 
+    text-sm 
+    h-8
+    bg-[#1677ff] 
+    text-[#fff] 
+    shadow-[0_2px_0_rgba(5,145,255,0.1)] 
+    text-center 
+    max-w-max 
+    px-4 
+    py-1 
+    rounded-md 
+    outline-none
+    hover:bg-[#4096ff]
+    active:bg-[#0958d9];
+  }
+</style>
---
+++ pages/article/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    class="p-12 h-full flex flex-col flex-grow min-h-[100vh]">
+    <!-- <CommonButton>publish now</CommonButton> -->
+    <div
+      id="vditor"
+      ref="vditorRef" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import Vditor from 'vditor';
+  import 'vditor/dist/index.css';
+
+  const vditorRef = ref<Vditor | null>(null);
+
+  onMounted(() => {
+    vditorRef.value = new Vditor('vditor', {
+      after: () => {
+        vditorRef.value?.setValue('');
+      },
+    });
+  });
+</script>
+
+<style scoped lang="scss">
+  #vditor {
+    height: 100% !important;
+    flex-grow: 1;
+  }
+</style>
